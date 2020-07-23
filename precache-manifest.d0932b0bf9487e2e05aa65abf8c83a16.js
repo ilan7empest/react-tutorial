4119918fb2c19255d6bbee2e9829947d18fb56dd
ilan7empest <ilank7@gmail.com>
2020-07-23 17:33:48 +0300

    Updates

---
+++ precache-manifest.d0932b0bf9487e2e05aa65abf8c83a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1506d53bb10223ede3d30a4440390443",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a9b6d7b459eaef8c02e0",
+    "url": "/react-tutorial/static/css/main.c33c711c.chunk.css"
+  },
+  {
+    "revision": "7c6a1891dd17bb44675a",
+    "url": "/react-tutorial/static/js/2.4d537e1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tutorial/static/js/2.4d537e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b6d7b459eaef8c02e0",
+    "url": "/react-tutorial/static/js/main.e0a6a45b.chunk.js"
+  },
+  {
+    "revision": "0aa4303aa3fe0fbe0b46",
+    "url": "/react-tutorial/static/js/runtime-main.f20b7db5.js"
+  }
+]);
